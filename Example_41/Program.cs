17fd739d58927a58a3b86a1b330a473650bb18f0
LarisaZaripova <larik7lolik@gmail.com>
2022-02-22 11:18:26 +0300

    added Example 48

--- Example_41/Program.cs
+++ Example_41/Program.cs
@@ -1,11 +1,16 @@
-﻿//Задача 48: Показать двумерный массив размером m×n заполненный целыми числами.
-int m = new Random().Next(1,4);
-int m = new Random().Next(1,4);
-int[,] matrix = new int[m, n];
+﻿// Задача 48: Показать двумерный массив размером m×n заполненный целыми числами.
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m,n];
 for(int i = 0; i < m; i++)
 {
     for(int j = 0; j < n; j++)
     {
-        Console.WriteLine($"{matrix[i, j]} ");
+        array[i,j] = new Random().Next(0,10);
+        Console.Write(array[i,j] + " ");
     }
+    Console.WriteLine();
 }
